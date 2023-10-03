350cd455b567238d0224a1976ac76ca567f526e7
Weronika Nowak <weronika.nowak@pollub.edu.pl>
2023-10-03 14:49:54 +0200

    Initial Commit

---
+++ ppppp/pppppApp.swift
@@ -0,0 +1,17 @@
+//
+//  pppppApp.swift
+//  ppppp
+//
+//  Created by student on 03/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct pppppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
